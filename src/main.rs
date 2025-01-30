1aa82765767975642506a08f52df4f8235a61c18
Kenny <27463495+Frontesque@users.noreply.github.com>
2025-01-30 09:21:08 -0500

    add help to syncboard

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,26 @@ mod client;
 pub mod clipboard;
 pub mod utils;
 
+fn help() {
+    println!("
+###   SyncBoard Help   ###
+1. syncboard server
+    - Starts SyncBoard in server mode
+
+2. syncboard client <ip_address>
+    - Starts SyncBoard in client mode and attempts to connect to a SyncBoard server running on the given <ip_address>
+
+3. syncboard test
+    - Tests SyncBoard capabilities on your system
+
+4. syncboard help
+    - Shows this message
+");
+}
+
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 { return eprintln!("Usage: {} <action>", args[0]); }
+    if args.len() < 2 { return help(); }
     
     println!("[SyncBoard] Welcome to SyncBoard v{}!", env!("CARGO_PKG_VERSION"));
     match args[1].as_str() {
@@ -15,7 +32,7 @@ fn main() {
         },
         "client" => {
             println!("[SyncBoard] Starting client...");
-            if args.len() < 3 { return eprintln!("Usage: {} client <ip_address>", args[0]); }
+            if args.len() < 3 { return help(); }
             let server_address = format!("ws://{}:9055",args[2]);
             client::start(server_address.to_string());
         },
@@ -27,7 +44,7 @@ fn main() {
             println!("[SyncBoard Test] Your clipboard should now contain: \"{}\".", test_string);
             println!("[SyncBoard Test] Try pasting somewhere!");
         }
-        _  => { return eprintln!("Usage: {} <action>", args[0]); }
+        _  => { return help(); }
     }
     println!("[SyncBoard] Exiting.");
 }
